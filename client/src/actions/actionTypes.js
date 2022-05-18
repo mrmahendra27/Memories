88c43d34466b227ed4224f06393873af0bbe62a6
MrMahendra27 <mkhati47@gmail.com>
2022-05-19 00:02:39 +0530

    cleaned code

---
+++ client/src/actions/actionTypes.js
@@ -0,0 +1,6 @@
+export const CREATE = 'CREATE';
+export const UPDATE = 'UPDATE';
+export const DELETE = 'DELETE';
+export const LIKE = 'LIKE';
+export const DISLIKE = 'DISLIKE';
+export const FETCH_ALL = 'FETCH_ALL';
