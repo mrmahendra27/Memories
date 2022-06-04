0e8913f23c42c5856104a163f008b2038f289e7f
MrMahendra27 <mkhati47@gmail.com>
2022-06-04 12:55:36 +0530

    Started Auth setup

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
+import React from "react";
+import { Link } from "react-router-dom";
+import Social from "../../images/social.png";
+import useStyles from "./styles";
+
+const Navbar = () => {
+  const classes = useStyles();
+  const user = null;
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <div className={classes.brandContainer}>
+        <Typography
+          component={Link}
+          to="/"
+          className={classes.heading}
+          variant="h2"
+          align="center"
+        >
+          Post Hub
+        </Typography>
+        <img className={classes.image} src={Social} alt="social" height="60" />
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.image_url}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.logout}
+              color="secondary"
+            >
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
